6714ee7a866d1c926966ea00185db2b8faf99363
Tim Perry <pimterry@gmail.com>
2019-01-24 17:14:44 +0100

    Move custom typings into a separate directory

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Handler } from 'aws-lambda';
-
-export const handler: Handler = async (event, context) => {
-    console.log('Received request', event);
-    return { statusCode: 200, body: 'Hello world' };
-}
--- src/paddle-webhook.ts
+++ src/paddle-webhook.ts
@@ -1,5 +1,5 @@
 import { Handler } from 'aws-lambda';
-import { NetlifyEvent } from './netlify-function-types';
+import { NetlifyEvent } from '../custom-typings/netlify-function-types';
 
 export const handler: Handler = async (event: NetlifyEvent, context) => {
     console.log('Received Paddle webhook', event);
