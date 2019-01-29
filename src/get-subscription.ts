c88536d540d712d73a384a14a68e061bb3e2c7c3
Tim Perry <pimterry@gmail.com>
2019-01-29 23:13:25 +0100

    Pull & sign live user app data from Auth0

--- src/get-subscription.ts
+++ src/get-subscription.ts
@@ -1,5 +1,13 @@
 import { Handler } from 'aws-lambda';
 import * as jwt from 'jsonwebtoken';
+import * as auth0 from 'auth0';
+
+const {
+    AUTH0_DOMAIN,
+    AUTH0_APP_CLIENT_ID,
+    AUTH0_MGMT_CLIENT_ID,
+    AUTH0_MGMT_CLIENT_SECRET
+} = process.env;
 
 const SIGNING_PRIVATE_KEY = `
 -----BEGIN RSA PRIVATE KEY-----
@@ -7,8 +15,36 @@ ${process.env.SIGNING_PRIVATE_KEY}
 -----END RSA PRIVATE KEY-----
 `;
 
+const authClient = new auth0.AuthenticationClient({
+    domain: AUTH0_DOMAIN,
+    clientId: AUTH0_APP_CLIENT_ID
+});
+
+const mgmtClient = new auth0.ManagementClient({
+    domain: AUTH0_DOMAIN,
+    clientId: AUTH0_MGMT_CLIENT_ID,
+    clientSecret: AUTH0_MGMT_CLIENT_SECRET
+});
+
+const BearerRegex = /^Bearer (\S+)$/;
+
 export const handler: Handler = async (event, context) => {
-    const appData = { test: true };
+    const { authorization } = event.headers;
+
+    const tokenMatch = BearerRegex.exec(authorization);
+    if (!tokenMatch) return { status: 401 };
+
+    const accessToken = tokenMatch[1];
+
+    // getProfile is only minimal data, updated at last login
+    const user: { sub: string } = await authClient.getProfile(accessToken);
+    // getUser is full live data for the user
+    const userData = await mgmtClient.getUser({ id: user.sub });
+
+    const appData = Object.assign(
+        { email: userData.email },
+        userData.app_metadata // undefined, for new users
+    );
 
     const signedAppData = jwt.sign(appData, SIGNING_PRIVATE_KEY, {
         algorithm: 'RS256',
@@ -17,6 +53,9 @@ export const handler: Handler = async (event, context) => {
         issuer: 'https://httptoolkit.tech/'
       });
 
-    console.log('Received get subscription request');
-    return { statusCode: 200, body: signedAppData };
+    return {
+        statusCode: 200,
+        headers: { 'content-type': 'application/jwt' },
+        body: signedAppData
+    };
 }
