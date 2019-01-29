ef1859325d8c4801a58fc722d7d6d4d84a8281bf
Tim Perry <pimterry@gmail.com>
2019-01-29 22:00:06 +0100

    Try to read private key

--- src/get-subscription.ts
+++ src/get-subscription.ts
@@ -1,6 +1,6 @@
 import { Handler } from 'aws-lambda';
 
 export const handler: Handler = async (event, context) => {
-    console.log('Received get subscription request', event, context, process.env);
+    console.log('Received get subscription request', event, context, process.env.SIGNING_PRIVATE_KEY);
     return { statusCode: 200, body: event.body };
 }
