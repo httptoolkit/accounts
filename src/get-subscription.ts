1eaa22c884bcad19403634f76f20873ef486f570
Tim Perry <pimterry@gmail.com>
2019-01-29 22:12:02 +0100

    Sign and return some demo data

--- src/get-subscription.ts
+++ src/get-subscription.ts
@@ -1,6 +1,22 @@
 import { Handler } from 'aws-lambda';
+import * as jwt from 'jsonwebtoken';
+
+const SIGNING_PRIVATE_KEY = `
+-----BEGIN RSA PRIVATE KEY-----
+${process.env.SIGNING_PRIVATE_KEY}
+-----END RSA PRIVATE KEY-----
+`;
 
 export const handler: Handler = async (event, context) => {
-    console.log('Received get subscription request', event, context, process.env.SIGNING_PRIVATE_KEY);
-    return { statusCode: 200, body: event.body };
+    const appData = { test: true };
+
+    const signedAppData = jwt.sign(appData, SIGNING_PRIVATE_KEY, {
+        algorithm: 'RS256',
+        expiresIn: '7d',
+        audience: 'https://httptoolkit.tech/app_metadata',
+        issuer: 'https://httptoolkit.tech/'
+      });
+
+    console.log('Received get subscription request');
+    return { statusCode: 200, body: signedAppData };
 }
