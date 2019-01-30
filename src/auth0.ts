3d051108619af033295118b67960b6f5b7602376
Tim Perry <pimterry@gmail.com>
2019-01-30 18:10:37 +0100

    Refactor auth0 setup into a separate file

---
+++ src/auth0.ts
@@ -0,0 +1,25 @@
+import * as auth0 from 'auth0';
+
+const {
+    AUTH0_DOMAIN,
+    AUTH0_APP_CLIENT_ID,
+    AUTH0_MGMT_CLIENT_ID,
+    AUTH0_MGMT_CLIENT_SECRET
+} = process.env;
+
+export const AUTH0_DATA_SIGNING_PRIVATE_KEY = `
+-----BEGIN RSA PRIVATE KEY-----
+${process.env.SIGNING_PRIVATE_KEY}
+-----END RSA PRIVATE KEY-----
+`;
+
+export const authClient = new auth0.AuthenticationClient({
+    domain: AUTH0_DOMAIN,
+    clientId: AUTH0_APP_CLIENT_ID
+});
+
+export const mgmtClient = new auth0.ManagementClient({
+    domain: AUTH0_DOMAIN,
+    clientId: AUTH0_MGMT_CLIENT_ID,
+    clientSecret: AUTH0_MGMT_CLIENT_SECRET
+});
--- src/get-app-data.ts
+++ src/get-app-data.ts
@@ -1,30 +1,7 @@
 import { Handler, APIGatewayProxyEvent } from 'aws-lambda';
 import * as jwt from 'jsonwebtoken';
-import * as auth0 from 'auth0';
 
-const {
-    AUTH0_DOMAIN,
-    AUTH0_APP_CLIENT_ID,
-    AUTH0_MGMT_CLIENT_ID,
-    AUTH0_MGMT_CLIENT_SECRET
-} = process.env;
-
-const SIGNING_PRIVATE_KEY = `
------BEGIN RSA PRIVATE KEY-----
-${process.env.SIGNING_PRIVATE_KEY}
------END RSA PRIVATE KEY-----
-`;
-
-const authClient = new auth0.AuthenticationClient({
-    domain: AUTH0_DOMAIN,
-    clientId: AUTH0_APP_CLIENT_ID
-});
-
-const mgmtClient = new auth0.ManagementClient({
-    domain: AUTH0_DOMAIN,
-    clientId: AUTH0_MGMT_CLIENT_ID,
-    clientSecret: AUTH0_MGMT_CLIENT_SECRET
-});
+import { AUTH0_DATA_SIGNING_PRIVATE_KEY, authClient, mgmtClient } from './auth0';
 
 const BearerRegex = /^Bearer (\S+)$/;
 
@@ -71,7 +48,7 @@ export const handler: Handler = async (event: APIGatewayProxyEvent) => {
         userData.app_metadata // undefined, for new users
     );
 
-    const signedAppData = jwt.sign(appData, SIGNING_PRIVATE_KEY, {
+    const signedAppData = jwt.sign(appData, AUTH0_DATA_SIGNING_PRIVATE_KEY, {
         algorithm: 'RS256',
         expiresIn: '7d',
         audience: 'https://httptoolkit.tech/app_data',
