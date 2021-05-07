0e3a6d49c0cd94575e00bd2eba0b7eddda9f175a
Tim Perry <pimterry@gmail.com>
2021-05-07 11:58:27 +0200

    Add script to manually cancelling an existing account

---
+++ scripts/cancel-account.ts
@@ -0,0 +1,26 @@
+import { mgmtClient } from '../api/src/auth0';
+
+// Cancel an account - closing the subscription and setting the expiry date to
+// now. This immediately ends any active subscription, useful for refunds or
+// migrating data to a different account etc.
+
+// This doesn't change any state in Paddle, so make sure any updates that need
+// to happen there are fired first!
+(async () => {
+    const email = process.argv[2];
+    console.log(`Cancelling account for ${email}`);
+
+    const users = await mgmtClient.getUsersByEmail(email);
+
+    if (users.length !== 1) {
+        console.error(`Unexpected found ${users.length} users - aborting`);
+        process.exit(1);
+    }
+
+    const userId = users[0].user_id!;
+
+    mgmtClient.updateAppMetadata({ id: userId }, {
+        subscription_status: 'deleted',
+        subscription_expiry: Date.now()
+    });
+})();
