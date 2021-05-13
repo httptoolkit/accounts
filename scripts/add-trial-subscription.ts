c9a61f6b5896290992e4002dbdc3050d98e135ad
Tim Perry <pimterry@gmail.com>
2021-05-13 14:11:55 +0200

    Create a script to more quickly set up trial subscriptions

---
+++ scripts/add-trial-subscription.ts
@@ -0,0 +1,49 @@
+import * as moment from 'moment';
+import { mgmtClient } from '../api/src/auth0';
+
+// Add a trial subscription in Auth0 for the target user. Occasionally useful
+// for actual custom trials in some cases, but mostly for open-source contributors
+// (Submit a useful PR or issue and get free HTTP Toolkit Pro!)
+(async () => {
+    const email = process.argv[2];
+    const durationString = process.argv[3];
+
+    const [, durationLength, durationType] = /(\d+)(\w+)/.exec(durationString);
+    const duration = moment.duration(
+        parseInt(durationLength, 10),
+        durationType as any
+    );
+
+    console.log(`Adding ${duration.asDays()} day subscription for ${email}`);
+
+    const users = await mgmtClient.getUsersByEmail(email);
+
+    let userId: string;
+    if (users.length === 1) {
+        if (
+            users[0].app_metadata?.subscription_status &&
+            users[0].app_metadata?.subscription_status !== 'deleted'
+        ) {
+            console.log("User already has subscription", users[0].app_metadata);
+        }
+
+        userId = users[0].user_id!;
+    } else if (users.length === 0) {
+        const user = await mgmtClient.createUser({
+            email,
+            email_verified: true,
+            connection: 'email'
+        });
+        userId = user.user_id!;
+    } else if (users.length > 1) {
+        console.error(`Unexpected found ${users.length} users - aborting`);
+        return process.exit(1);
+    }
+
+    mgmtClient.updateAppMetadata({ id: userId }, {
+        subscription_status: 'trialing',
+        subscription_plan_id: 550380, // Pro monthly
+        subscription_expiry: Date.now() + duration.asMilliseconds(),
+        subscription_id: -1
+    });
+})();
