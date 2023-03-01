d87492d66a76328e50447dafaa624c510cec30db
Tim Perry <pimterry@gmail.com>
2023-03-01 20:15:46 +0100

    Create a PayPro thank you page redirect

---
+++ api/src/functions/redirect-paypro-to-thank-you.ts
@@ -0,0 +1,28 @@
+import { initSentry, catchErrors, reportError } from '../errors';
+initSentry();
+
+import * as querystring from 'querystring';
+
+interface PostCheckoutParams {
+    ORDER_CUSTOM_FIELDS?: string;
+}
+
+export const handler = catchErrors(async (event) => {
+    const checkoutData = querystring.parse(event.body || '') as PostCheckoutParams;
+    const customParams = new URLSearchParams(checkoutData.ORDER_CUSTOM_FIELDS ?? '');
+
+    let returnUrl = customParams.get('x-return-url');
+    if (!returnUrl) {
+        reportError('Received paypro-thank-you without a return URL defined');
+        returnUrl = "https://httptoolkit.com/web-purchase-thank-you/";
+    } else if (!returnUrl.match(/^https:\/\/httptoolkit.com\/(web|app)-purchase-thank-you/)) {
+        reportError(`Received paypro-thank-you with an unrecognized return URL: ${returnUrl}`);
+        returnUrl = "https://httptoolkit.com/web-purchase-thank-you/";
+    }
+
+    return {
+        statusCode: 302,
+        headers: { location: returnUrl },
+        body: ''
+    };
+});
