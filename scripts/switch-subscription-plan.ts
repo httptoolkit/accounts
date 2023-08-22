91ad3de5d6b726ee8da99cb4e2e920824fbc8701
Tim Perry <pimterry@gmail.com>
2023-08-22 11:13:03 +0100

    Add a switch-plan script

---
+++ scripts/switch-subscription-plan.ts
@@ -0,0 +1,84 @@
+import fetch from 'node-fetch';
+import * as prompts from 'prompts';
+
+import { mgmtClient } from '../api/src/auth0';
+
+const {
+    PADDLE_ID,
+    PADDLE_KEY
+} = process.env;
+
+const email = process.argv[2];
+const newPlan = process.argv[3]
+const price = +process.argv[4];
+const currency = process.argv[5];
+
+(async () => {
+    if (isNaN(price)) {
+        throw new Error(`Price must be a number, was ${price}`);
+    }
+
+    if (!currency || currency.length !== 3) {
+        throw new Error('Currency must be a 3-letter code, e.g. EUR');
+    }
+
+    const users = await mgmtClient.getUsersByEmail(email);
+
+    if (users.length !== 1) {
+        throw new Error(`Can't update, found ${users.length} users for email ${email}`);
+    }
+
+    const user = users[0];
+
+    if (
+        !user.app_metadata ||
+        user.app_metadata.subscription_status !== 'active' ||
+        user.app_metadata.subscription_expiry < Date.now()
+    ) {
+        throw new Error(`User has no active subscription. Data is: ${user.app_metadata}`);
+    }
+
+    const subscriptionId = user.app_metadata.subscription_id;
+    const existingQuantity = user.app_metadata.subscription_quantity;
+
+    const { result } = await prompts({
+        name: 'result',
+        type: 'confirm',
+        message: `Update subscription https://vendors.paddle.com/subscriptions/customers/manage/${subscriptionId} to ${
+            newPlan
+        } at ${
+            price
+        } ${
+            currency
+        }?`
+    });
+
+    if (!result) {
+        console.log("Cancelled");
+        process.exit(1);
+    }
+
+    const response = await fetch('https://vendors.paddle.com/api/2.0/subscription/users/update', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+            vendor_id: PADDLE_ID!,
+            vendor_auth_code: PADDLE_KEY!,
+            subscription_id: subscriptionId,
+            quantity: existingQuantity ?? 1,
+            plan_id: newPlan,
+            currency: currency,
+            recurring_price: price.toString()
+        }).toString()
+    });
+
+    if (!response.ok) {
+        console.error(`Unexpected ${response.status} response`);
+        response.body.pipe(process.stderr);
+        response.body.on('end', () => process.exit(1));
+    } else {
+        response.body.pipe(process.stdout);
+    }
+})();
