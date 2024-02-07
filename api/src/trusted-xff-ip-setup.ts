5093a363134e62abebb28b47f0b9279ddd7aff70
Tim Perry <pimterry@gmail.com>
2024-02-07 17:07:07 +0100

    Improve CDN IP loading with an on-disk startup cache

---
+++ api/src/trusted-xff-ip-setup.ts
@@ -0,0 +1,68 @@
+import type { Application } from 'express';
+import * as ipAddr from 'ipaddr.js';
+
+import { reportError } from './errors';
+
+// We need to know our traffic sources to be able to know when to trust the X-Forwarded-For header,
+// so that we can accurately work out the original IP source of incoming requests. We trust local
+// reverse proxies and Scaleway's container proxy, but we also need to trust our CDN.
+
+// Unfortunately Bunny (our CDN) uses IPs that may change, so we have dynamically update those.
+// We handle this with a cached file generated on each container build, and a subsequent interval
+// to update dynamically later on.
+
+const TRUSTED_IP_SOURCES = [
+    'loopback',
+    'uniquelocal',
+    '100.64.0.0/10', // Private network shared address space, used by Scaleway
+];
+
+let bunnyCachedIPs: string[] = [];
+try {
+    bunnyCachedIPs.push(...require('../.bunny-ipv4-ips.json'))
+} catch {}
+try {
+    bunnyCachedIPs.push(...require('../.bunny-ipv6-ips.json'))
+} catch {}
+
+export function configureAppProxyTrust(app: Application) {
+    console.log(`Loaded ${bunnyCachedIPs.length} CDN IPs from disk`);
+    app.set('trust proxy', [
+        ...TRUSTED_IP_SOURCES,
+        ...bunnyCachedIPs.filter(ip => ipAddr.isValid(ip))
+    ]);
+
+    async function updateTrustedProxySources() {
+        try {
+            const [
+                bunnyIPv4s,
+                bunnyIPv6s
+            ] = (await Promise.all<Array<string>>([
+                fetch('https://bunnycdn.com/api/system/edgeserverlist').then(r => r.json()),
+                fetch('https://bunnycdn.com/api/system/edgeserverlist/IPv6').then(r => r.json())
+            ]));
+
+            const bunnyIPs = [
+                ...bunnyIPv4s,
+                ...bunnyIPv6s
+            ].filter(ip => ipAddr.isValid(ip));
+
+            app.set('trust proxy', [
+                ...TRUSTED_IP_SOURCES,
+                ...bunnyIPv4s,
+                ...bunnyIPv6s
+            ]);
+
+            console.log(`Updated to trust ${bunnyIPs.length} CDN IPs`);
+        } catch (e: any) {
+            console.log(e);
+            reportError(`Failed to update CDN IPs: ${e.message || e}`);
+            // Retry every minute until success:
+            setTimeout(updateTrustedProxySources, 1000 * 10);
+        }
+    }
+
+    // On startup, and then every hour, update trusted Bunny CDN IPs:
+    updateTrustedProxySources();
+    setInterval(updateTrustedProxySources, 1000 * 60 * 60).unref();
+}
