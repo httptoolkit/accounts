d8092c36106b32ae36a20eea9282d681fae01a27
Tim Perry <pimterry@gmail.com>
2026-01-08 15:23:59 +0100

    Update initial schema with indexes for existing usage patterns

--- api/src/db/migrations/1767376348060_init.ts
+++ api/src/db/migrations/1767376348060_init.ts
@@ -1,4 +1,4 @@
-import type { Kysely } from 'kysely'
+import { sql, type Kysely } from 'kysely'
 
 export async function up(db: Kysely<any>): Promise<void> {
 	const { schema } = db;
@@ -6,7 +6,22 @@ export async function up(db: Kysely<any>): Promise<void> {
 	await schema.createTable('users')
 		.addColumn('id', 'serial', (col) => col.primaryKey())
 		.addColumn('auth0_user_id', 'text', (col) => col.unique())
-		.addColumn('email', 'text', (col) => col.notNull())
+		.addColumn('email', 'text', (col) => col.notNull().unique())
 		.addColumn('app_metadata', 'jsonb', (col) => col.notNull())
 		.execute();
+
+	await Promise.all([
+		schema.createIndex('user_email_index')
+			.on('users')
+			.columns(['email'])
+			.execute(),
+		schema.createIndex('user_auth0_user_id_index')
+			.on('users')
+			.columns(['auth0_user_id'])
+			.execute(),
+		schema.createIndex('user_app_metadata_subscription_owner_id_index')
+			.on('users')
+			.expression(sql`(app_metadata->>'subscription_owner_id')`)
+			.execute()
+	]);
 }
